ad1ca8ba6adafa03deea5e1f31270b33d8af6b8c
fidel bello <59423@bears.smcc.edu>
2021-03-10 21:56:24 -0500

    created db tables for sql

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+/*delete database if exists, creates database, uses the database*/
+DROP DATABASE IF EXISTS emptracker_db;
+CREATE DATABASE emptracker_db;
+USE emptracker_db;
+
+/*department table*/
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAr(30)
+);
+
+/*role table*/
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+/*employee table*/
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT, 
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
+
+);
--- seeds.sql
+++
@@ -1,4 +0,0 @@
-/*delete database if exists, creates database, uses the database*/
-DROP DATABASE IF EXISTS emptracker_db;
-CREATE DATABASE emptracker_db;
-USE emptracker_db;
