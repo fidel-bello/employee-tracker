4eb96e17a1146e825619f8dde80daf96defe433b
fidel bello <59423@bears.smcc.edu>
2021-03-13 18:35:10 -0500

    made connection to mysql and made startapp function

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+//node modules
 const connection = require('./config/connection');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
+
+//connection to mysql, if not console error
 connection.connect((err) => {
     if(err) {
         console.log((err));
         return;
     }
-    console.log(`Connected to ThreadID: ${connection.threadID}`);
-    connection.end()
+    console.log("Welcome!");
+    //calling function to start app
+    startApp()
 });
+//starting inquirer 
+function startApp(){
+    inquirer.prompt({})
+}
