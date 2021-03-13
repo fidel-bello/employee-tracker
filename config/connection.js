2ddd9c8469617994eaaf21930e31decb4b9c57dd
fidel bello <59423@bears.smcc.edu>
2021-03-13 18:55:46 -0500

    added comments to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,13 @@
+//node modules
 const mysql = require('mysql');
 require('dotenv').config()
+
+//connection to mysql with env file
 const connection = mysql.createConnection({
-    
     user: process.env.USER,
     password: process.env.PASSWORD,
     host: process.env.HOST,
-    database: process.env.DATABASE
-    
+    database: process.env.DATABASE  
 });
 
 module.exports = connection;
