f029637fc27d0288b2fc42046f15efd179537db5
fidel bello <59423@bears.smcc.edu>
2021-03-15 21:33:07 -0400

    added view all roles function

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function startApp(){
             case "View All Employees?":
                 viewAll();
             break;
-            case "View Alll Employee's By Roles?":
+            case "View All Employee's By Roles?":
                 viewAllRoles();
             break;
 
@@ -58,17 +58,21 @@ function startApp(){
         }
     })
 }
-
+//--- view all employees with all roles and dept ---// 
 function viewAll() {
     connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
      function (err, res){
          if (err) throw err;
-         console.table(res);
+         console.Ctable(res);
          startApp();
      })
 };
-
+// --- view all employees by just roles ---//
 function viewAllRoles(){
-    
-}
- 
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;",
+    function (err, res){
+        if(err) throw err;
+        console.table(res);
+        startApp();
+    })
+};
